2a079599ec19e4dc6ce0eacc42883515d2b3b4ec
Nikos Mavrakis <mavrakis.n@gmail.com>
2019-03-06 20:52:32 +0200

    Add support for easy_thumbnails with Jinja2

--- project_name/jinja2/methods.py
+++ project_name/jinja2/methods.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from django.urls import reverse
 from django.utils.translation import get_language_info as gli
 
+# from easy_thumbnails.templatetags import thumbnail as _thumbnail
+
 from {{ project_name }}.urls import urlpatterns
 
 
@@ -29,3 +31,24 @@ def app_urls():
         url.name: reverse(url.name) for url in urlpatterns if hasattr(url, "name")
     }
 
+
+def thumbnail(source, **kwargs):
+    """
+    If easy_thumbnails is installed, use it with Jinja2 as follows:
+
+    {% set thumb = thumbnail(obj.ImageFieldName, size=(300, 300), quality=75) %}
+	<img
+	  src="{{ thumb.url }}"
+	  alt="..."
+	  width="{{ thumb.width }}"
+	  height="{{ thumb.height }}"
+	>
+
+	Line 'kwargs["crop"] = ...' may be deleted if "crop" is not the default.
+
+    :param source: an ImageField object
+    :param kwargs: size (tuple), crop (bool), quality (int)
+    :return: thumbnail object
+    """
+    kwargs["crop"] = kwargs.get("crop", True)
+    return _thumbnail.get_thumbnailer(source).get_thumbnail(kwargs)
